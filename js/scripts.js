a029f51f8a3a6e3d2f03d594037b48fe23aac72c
craraulsalazar <raul.salazar2008@gmail.com>
2015-09-23 02:17:16 -0400

    version 3
    fixing issue with loading markers after map loaded

--- js/scripts.js
+++ js/scripts.js
@@ -4,35 +4,35 @@ $(document).ready(function(){/* google maps ------------------------------------
 
     if (typeof google !== 'object') {
 
-        //display friendly error message
-        //alert('google map failed to load. check you internet connection');
+        //display friendly error message, whenever google maps or internet is down
 
         $('#map-canvas').append("<div class='alert alert-danger'>Unable to load Google Maps - No Internet Available</div>");
 
         return;
     }
 
+    //add listener to initialize google map
     google.maps.event.addDomListener(window, 'load', initialize);
 
+
+    //set global variables
     var map;
     var markers = [];
 
     var infowindow = null;
 
+
     function initialize() {
 
-
-  /* position Amsterdam */
-  //var latlng = new google.maps.LatLng(52.3731, 4.8922);
+        //set initial position to city of toronto
   var latlng = new google.maps.LatLng(43.6425662,-79.3870568);
         //var latlng = new google.maps.LatLng(43.82034693693644,-79.18190002441406);
-
 
     var mapOptions = {
     center: latlng,
     scrollWheel: false,
     zoom: 14
-  };
+  }
 
 /*
   var marker = new google.maps.Marker({
@@ -42,30 +42,53 @@ $(document).ready(function(){/* google maps ------------------------------------
   });
 */
 
+  //getting id where we the map will be loaded and load global variable
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-  //marker.setMap(map);
+         google.maps.event.addListenerOnce(map, 'idle', function(){
+                //map is fully loaded. set all markers in map
 
-        //markers.push(marker);
+                DisplayAllPlaces(locationstovisit);
+         });
 
 
 
-};
 
+    };
+
+    //This function will create and position the markers in the map
     function CreateMarkers(geodata, itemclickedfromlist){
 
         var marker;
-        console.log('calling createmarkers');
+        //console.log('calling createmarkers');
 
-        console.log(map);
+        //console.log(map);
 
             var venue = geodata.response.venue;
             var location = venue.location;
             var lat = location.lat;
             var lng = location.lng;
 
-        console.log('venue name: ' + venue.name);
-        console.log(venue);
+        //console.log('venue name: ' + venue.name);
+        //console.log(venue);
+
+        if( typeof itemclickedfromlist !== 'undefined') {
+
+            //
+            console.log('user click button');
+            for (var i = 0; i < markers.length; i++) {
+
+                if (markers[i].title == venue.name){
+
+                    console.log('item remove from map: ' + markers[i].title);
+
+                    markers[i].setMap(null);
+                    markers.splice(i, 1);
+
+                    break;
+                }
+            }
+        }
 
         marker = new google.maps.Marker({
                     position: new google.maps.LatLng(lat, lng),
@@ -76,8 +99,6 @@ $(document).ready(function(){/* google maps ------------------------------------
 
 
         var address = location.address;
-        var lat = location.lat;
-        var lng = location.lng;
         var city = location.city;
         var postal = location.postalCode;
 
@@ -87,7 +108,6 @@ $(document).ready(function(){/* google maps ------------------------------------
         var photos = venue.photos.groups[0].items;
         var bestphoto = venue.bestPhoto;
 
-        //https://developers.google.com/maps/documentation/javascript/examples/infowindow-simple
 
         marker.addListener('click', function() {
 
@@ -126,13 +146,10 @@ $(document).ready(function(){/* google maps ------------------------------------
                 content: contentString
             });
 
-            marker.setAnimation(google.maps.Animation.BOUNCE)
+            marker.setAnimation(google.maps.Animation.BOUNCE);
 
-            //var locaddr = address + ',' + city;
 
-            //var googleviewurl = 'https://maps.googleapis.com/maps/api/streetview?size=400x400&location=' + lat + ',' + lng + '';
-
-                ShowImages(photos,bestphoto);
+            ShowImages(photos,bestphoto);
 
 
             infowindow.open(map, marker);
@@ -148,13 +165,17 @@ $(document).ready(function(){/* google maps ------------------------------------
             ShowImages(photos,bestphoto);
 
             //http://jsfiddle.net/sgentile/pRC4c/
-
+            //trigger marker click event
+            google.maps.event.trigger(marker, 'click');
         }
+
+        //check to see if marker has been already pin in map
+        //if so, just don't added to the markers array
 
         markers.push(marker);
 
         var bounds = new google.maps.LatLngBounds();
-        for(i=0;i<markers.length;i++) {
+        for(var i=0;i<markers.length;i++) {
             bounds.extend(markers[i].getPosition());
         }
 
@@ -175,45 +196,49 @@ $(document).ready(function(){/* google maps ------------------------------------
             map.setZoom(15);
         }
 
-    };
+    }
 
+    //This function will display images from foursquare
     function ShowImages(photos,bestphoto)
     {
-
+        //remove previous pictures
         $('.bgimg').remove();
 
-        photoslen = photos.length;
+        //load pictures from photos variable
+        var photoslen = photos.length;
         for(var i=0; i< photoslen-1;i++ ) {
             var photourl = photos[i].prefix + '100x100' + photos[i].suffix;
             $('#locimg').append('<div class="col-md-4 col-xs-6 col-lg-2"><img class="bgimg" src="' + photourl + '" class="img-responsive"></div>');
         }
 
-        //venue picture
+        //load picture from bestphoto variable
         var venueimage = bestphoto.prefix + '100x100' + bestphoto.suffix;
         $('#locimg').append('<div class="col-md-4 col-xs-6 col-lg-2"><img class="bgimg" src="' + venueimage + '" class="img-responsive"></div>');
 
 
     }
 
-
+    //This function will clear all markers
     function clearMarkers() {
 
         for (var i = 0; i < markers.length; i++) {
             markers[i].setMap(null);
-
         }
+
+        markers = [];
 
     }
 
+    //This function will Load all details from foursquare API
     function LoadFourSquareDetails(place, itemclickedfromlist)
     {
 
-        //http://stackoverflow.com/questions/16554748/adding-multiple-markers-to-google-map
-        //remove all markers
-        if (markers.length> 0)
+
+        //remove all markers only when itemclickedfromlist parameter is empty
+        if (markers.length> 0 && typeof itemclickedfromlist === 'undefined')
         {
-            //https://developers.google.com/maps/documentation/javascript/examples/marker-remove
             clearMarkers();
+
         }
 
         var options = {
@@ -225,14 +250,16 @@ $(document).ready(function(){/* google maps ------------------------------------
         $.ajax(options)
             .done(function( data ) {
 
-                var venue = data.response.venue;
+                //var venue = data.response.venue;
                 CreateMarkers(data,itemclickedfromlist);
             }
         ).fail(
             function(data){
-                //alert( "fail: " + data );
+                //clear previous images or alerts
                 $('.bgimg').remove();
                 $('.alert').remove();
+
+                //show alert in case internet is down
                 $('#locimg').append("<div class='alert alert-danger'>Unable to call API - No Internet Available</div>");
             }
         );
@@ -240,6 +267,26 @@ $(document).ready(function(){/* google maps ------------------------------------
     }
 
 
+    //this function will look for all my favorites places to go and set the markers
+    var DisplayAllPlaces = function (places) {
+        // do something with newValue, like an Ajax request.
+
+        viewModel.currentlocationselected('');
+        var allplacestovisitlen = places.length;
+
+        for(var i=0; i< allplacestovisitlen;i++ )
+        {
+            var place = places[i];
+
+            console.log('url: '+ place.url);
+
+            LoadFourSquareDetails(place);
+
+        }
+    }
+
+
+//my favorites places to visit in toronto
   var locationstovisit = [
         {
           name:"CN Tower",
@@ -270,6 +317,7 @@ $(document).ready(function(){/* google maps ------------------------------------
       ;
 
 
+    //build viewmodel using MVVM Model with knowoutjs
   var viewModel = {
 
     query: ko.observable(''),
@@ -295,14 +343,10 @@ $(document).ready(function(){/* google maps ------------------------------------
 
   };
 
-
-//http://stackoverflow.com/questions/17344935/select-an-item-in-a-list-on-click-with-knockout
-
   viewModel.locations = ko.dependentObservable(function() {
     var search = this.query().toLowerCase();
 
-    //clear pics
-
+    
     return ko.utils.arrayFilter(locationstovisit, function(loc) {
 
         if (markers.length> 0)
@@ -315,6 +359,9 @@ $(document).ready(function(){/* google maps ------------------------------------
       return loc.name.toLowerCase().indexOf(search) >= 0;
     });
   }, viewModel);
+
+
+    viewModel.locations.subscribe(DisplayAllPlaces);
 
     /*
     viewModel.locations.subscribe(function (places) {
@@ -332,11 +379,9 @@ $(document).ready(function(){/* google maps ------------------------------------
             LoadFourSquareDetails(place);
 
         }
-
-
     });
-    */
 
+    */
 
   ko.applyBindings(viewModel);
 
